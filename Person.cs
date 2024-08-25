96e15c101e032df3fdd1af9e7e069db51c4d65cb
Danna-Melisa <danna.acevedo252@pascualbravo.edu.co>
2024-08-24 19:34:38 -0500

    first commit

---
+++ Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    internal class Person
+    {
+        private int idPerson;
+        private string firstname;
+        private string lastname;
+        private int age;
+        private double heigth;
+        private string gender;
+        private string phone;
+        private string email;
+
+        #region constructores 
+        // Constructor por defecto
+        public Person() { }
+
+        public Person(int idPerson, string firstname, string lastname, int age, double heigth, string gender, string phone, string email)
+        {
+            this.IdPerson = idPerson;
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Age = age;
+            this.Heigth = heigth;
+            this.Gender = gender;
+            this.Phone = phone;
+            this.Email = email;
+        }
+        // Crear encapsulamiento 
+        public int IdPerson { get => idPerson; set => idPerson = value; }
+        public string Firstname { get => firstname; set => firstname = value; }
+        public string Lastname { get => lastname; set => lastname = value; }
+        public int Age { get => age; set => age = value; }
+        public double Heigth { get => heigth; set => heigth = value; }
+        public string Gender { get => gender; set => gender = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string Email { get => email; set => email = value; }
+        #endregion
+
+        #region CREAR METODOS
+
+        public void ViewData()
+            {
+              Console.WriteLine("                             ");
+              Console.WriteLine("  *** DATOS DE LA PERSONA ***");
+              Console.WriteLine("                             ");
+              Console.WriteLine($"\tIdentificacion-> {idPerson}");
+              Console.WriteLine($"\tNombre --------> {firstname}");
+              Console.WriteLine($"\tApellido-------> {lastname}");
+              Console.WriteLine($"\tEdad-----------> {age}");
+			  Console.WriteLine($"\tEstatura-------> {heigth}");
+			  Console.WriteLine($"\tGenero---------> {gender}");
+			  Console.WriteLine($"\tCelular--------> {phone}");
+			  Console.WriteLine($"\tCorreo---------> {email}");
+			  Console.WriteLine("|________________________|\n\n");
+
+		}
+		#region 
+		public void UpdateGender(string genero)
+		{
+			this.gender = genero;
+		
+		}
+        #endregion
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region utilizando el constructor por defecto 
+            //instanciar , crear un objeto a partir de la class
+            Person person1 = new Person();
+            person1.IdPerson = 123456789;
+            person1.Firstname = "Paco";
+
+            Console.WriteLine("Cedula -->" + person1.IdPerson);
+			Console.WriteLine("Nombre -->" + person1.Firstname);
+			Console.ReadKey();
+			#endregion
+		}
+		static void Main(string[] args)
+		{
+			#region utilizando el constructor por defecto 
+			//instanciar , crear un objeto a partir de la class
+			Person person1 = new Person(1235, "Sara", "Gomez", 18, 1.60, "Femenino", "300300300300", "saragomez@.com");
+			Person person2 = new Person(1235, "Felipe", "Serbia", 20, 1.80, "Masculino", "300300300300", "felipe.serbia@.com");
+			Person person3 = new Person(1235, "Melisa", "Acevedo", 17, 1.55, "Femenino", "300300300300", "melisaacevedo@.com");
+
+			person1.ViewData();
+			person2.ViewData();
+			person3.ViewData();
+
+			person1.UpdateGender("No binario");
+
+
+			Console.ReadKey();
+
+			#endregion
+
+			Console.WriteLine("Desea actualizar datos? presione 1.Si  2.No ");
+			string input = Console.ReadLine();
+
+            int opcion;
+
+			while (!int.TryParse(input, out opcion) || (opcion != 1 && opcion != 2))
+			{
+				Console.WriteLine("Entrada no válida. Por favor ingrese 1 para Si o 2 para No.");
+				input = Console.ReadLine();
+			}
+
+			if break == 2; 
+
+
+		}
+	}
+
+	public void ModificarDato()
+	{
+
+		if()
+
+	}
+
+}
